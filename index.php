4213609c4eaa46e6adf6044f3e2f059e081e2709
dvthnight <davide.lanotte@protonmail.com>
2022-04-08 14:52:39 +0200

    creata una public function nella classe

--- index.php
+++ index.php
@@ -11,10 +11,27 @@
             $this->attori = $_attori;
             $this->lingua_originale = $_lingua_originale;
             $this->regista = $_regista;
-            $this->anno = $_anno;
+            $this-> setAnno($_anno);
+        }
+
+
+        public function setAnno($_anno){
+            if(is_numeric($_anno)){
+                $this->anno = $_anno;
+            };
         }
     }
 
-    $red_2 = new Movie("Red 2", "Bruce Willis","inglese","Dean Parisot","2013");
-    // $red_2->titolo = "Red 2";
+    $red_2 = new Movie("Red 2", ["Bruce Willis","John Malkovich","Catherine Zeta Jones"],"inglese","Dean Parisot","2013");
     var_dump($red_2);
+
+    $top_gun = new Movie("Top Gun",["Tom Cruise","Kelly McGillis","Anthony Edwards"],"inglese","Tony Scott","1986");
+    var_dump($top_gun);
+
+    $creed = new Movie("Creed",["Michael B. Jordan","Sylvester Stallone","Sylvester Stallone"],"inglese","Ryan Coogler","2015");
+    var_dump($creed);
+
+    $simpson = new Movie("I Simpson - Il film",["Homer","Marge","Bart","Lisa","Maggie"],"inglese","David Silverman","come mai");
+    var_dump($simpson);
+
+    
